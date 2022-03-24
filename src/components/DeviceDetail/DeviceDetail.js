20337fc86caa9b6c34140230210e5c985e84e2d5
khadizaliza <khadizaakter19836@gmail.com>
2022-03-24 17:43:05 +0600

    first commit

---
+++ src/components/DeviceDetail/DeviceDetail.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+
+const DeviceDetail = (props) => {
+    return (
+        <div>
+           <p>Price: {props.price}</p> 
+        </div>
+    );
+};
+
+export default DeviceDetail;
+
+
+
+
+
+
---
+++ src/components/Display/Display.js
@@ -0,0 +1,24 @@
+
+import React from 'react';
+import Dial from '../Dial/Dial';
+const Display = (props) => {
+    return (
+        <div style={{border: '2px solid green', margin: '20px'}}>
+            <h2>Name: {props.name}</h2>
+            <p>So far steps today: {props.steps}</p>
+            <Dial steps={props.steps}></Dial>
+        </div>
+    );
+};
+
+export default Display;
+
+
+
+
+
+
+
+
+
+
